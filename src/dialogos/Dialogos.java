508b78e23d67c74b80c15a14bd4f2132cf21c5dd
artur <artur@ArturoMSI>
2018-01-19 08:56:08 +0100

    Añadido Metodo para recoger textos mostrando un mensaje para pedirlos

--- src/dialogos/Dialogos.java
+++ src/dialogos/Dialogos.java
@@ -8,7 +8,26 @@ import javax.swing.JOptionPane;
  */
 public class Dialogos{
 
+    /**
+     * Metodo para mostrar un mensaje en pantalla
+     *
+     * @param mensaje
+     */
     public static void mensaje(String mensaje){
-        JOptionPane.showInputDialog(mensaje);
+        JOptionPane.showMessageDialog(null, mensaje);
     }
+
+    /**
+     * Metodo que recoje y devuelve una palabra o palabras mostrando en pantalla
+     * el mensaje deseado
+     *
+     * @param mensaje
+     * @return Devuelve el String introducido
+     */
+    public static String recogerString(String mensaje){
+        String dato;
+        dato=JOptionPane.showInputDialog(mensaje);
+        return dato;
+    }
+
 }

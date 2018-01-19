fd23c14dc46c2afd5757247307170d1aba53dae0
artur <artur@ArturoMSI>
2018-01-19 08:47:38 +0100

    Añadido Metodo para mostrar mensajes

---
+++ src/dialogos/Dialogos.java
@@ -0,0 +1,14 @@
+package dialogos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Arturo
+ */
+public class Dialogos{
+
+    public static void mensaje(String mensaje){
+        JOptionPane.showInputDialog(mensaje);
+    }
+}
